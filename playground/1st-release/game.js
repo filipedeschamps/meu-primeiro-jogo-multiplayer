a016ea2f1022a4cd7b6d086121055cb262aef8e5
Filipe Deschamps <filiped@gmail.com>
2019-12-06 17:56:41 +0000

    client: split code into modules

---
+++ playground/1st-release/game.js
@@ -0,0 +1,103 @@
+export default function createGame() {
+    const state = {
+        players: {},
+        fruits: {},
+        screen: {
+            width: 10,
+            height: 10
+        }
+    }
+
+    function addPlayer(command) {
+        const playerId = command.playerId
+        const playerX = command.playerX
+        const playerY = command.playerY
+
+        state.players[playerId] = {
+            x: playerX,
+            y: playerY
+        }
+    }
+
+    function removePlayer(command) {
+        const playerId = command.playerId
+
+        delete state.players[playerId]
+    }
+
+    function addFruit(command) {
+        const fruitId = command.fruitId
+        const fruitX = command.fruitX
+        const fruitY = command.fruitY
+
+        state.fruits[fruitId] = {
+            x: fruitX,
+            y: fruitY
+        }
+    }
+
+    function removeFruit(command) {
+        const fruitId = command.fruitId
+
+        delete state.fruits[fruitId]
+    }
+
+    function movePlayer(command) {
+        const acceptedMoves = {
+            ArrowUp(player) {
+                if (player.y - 1 >= 0) {
+                    player.y = player.y - 1
+                }
+            },
+            ArrowRight(player) {
+                if (player.x + 1 < state.screen.width) {
+                    player.x = player.x + 1
+                }
+            },
+            ArrowDown(player) {
+                if (player.y + 1 < state.screen.height) {
+                    player.y = player.y + 1
+                }
+            },
+            ArrowLeft(player) {
+                if (player.x - 1 >= 0) {
+                    player.x = player.x - 1
+                }
+            }
+        }
+
+        const keyPressed = command.keyPressed
+        const playerId = command.playerId
+        const player = state.players[playerId]
+        const moveFunction = acceptedMoves[keyPressed]
+
+        if (player && moveFunction) {
+            moveFunction(player)
+            checkForFruitCollision(playerId)
+        }
+
+    }
+
+    function checkForFruitCollision(playerId) {
+        const player = state.players[playerId]
+
+        for (const fruitId in state.fruits) {
+            const fruit = state.fruits[fruitId]
+            console.log(`Checking ${playerId} and ${fruitId}`)
+
+            if (player.x === fruit.x && player.y === fruit.y) {
+                console.log(`COLLISION between ${playerId} and ${fruitId}`)
+                removeFruit({ fruitId: fruitId })
+            }
+        }
+    }
+
+    return {
+        addPlayer,
+        removePlayer,
+        movePlayer,
+        addFruit,
+        removeFruit,
+        state
+    }
+}
---
+++ playground/1st-release/keyboard-listener.js
@@ -0,0 +1,32 @@
+export default function createKeyboardListener(document) {
+    const state = {
+        observers: []
+    }
+
+    function subscribe(observerFunction) {
+        state.observers.push(observerFunction)
+    }
+
+    function notifyAll(command) {
+        for (const observerFunction of state.observers) {
+            observerFunction(command)
+        }
+    }
+
+    document.addEventListener('keydown', handleKeydown)
+
+    function handleKeydown(event) {
+        const keyPressed = event.key
+
+        const command = {
+            playerId: 'player1',
+            keyPressed
+        }
+
+        notifyAll(command)
+    }
+
+    return {
+        subscribe
+    }
+}
