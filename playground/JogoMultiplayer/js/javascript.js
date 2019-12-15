2455b77624fc6ca4a77f6048bb00701754cc2ce0
zblast5000 <amorimdanilom@gmail.com>
2019-12-15 15:40:15 +0000

    novo

--- playground/JogoMultiplayer/js/javascript.js
+++ playground/JogoMultiplayer/js/javascript.js
@@ -3,11 +3,35 @@
 const screen = document.getElementById('screen')
 const context = screen.getContext('2d')
 
-const color = 'red'
-const positionX = 0
-const positionY = 0
-const width = 250
-const heigh = 250
 
-context.fillStyle = color
-context.fillRect(positionX,positionY, width, heigh)
+// dados jogo
+
+const game = {
+    players: {
+        'player1': {x: 1 , y:1 },
+        'player2': {x: 9, y: 9}
+    },
+    fruits: {
+        'fruit1':{x: 3, y:1}
+    }
+}
+
+renderScreen()
+
+function renderScreen(){
+    for (playerId in players){
+        const player = game.players[playerId]
+        context.fillStyle = 'black'
+        context.fillRect(player.x,player.y, 1, 1)
+
+    }
+    for (fuitId in fruits){
+        const fruit = game.fruits[fruitId]
+        context.fillStyle = 'green'
+        context.fillStyle(fruit.x,fruit.y,1,1)
+        
+    }
+
+}
+
+
