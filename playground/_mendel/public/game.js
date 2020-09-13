93e40ca50f5214852d2037753a4f02a5a395a335
guiMendel <guimendeln@gmail.com>
2020-09-13 13:48:03 +0000

    Limite de frutas no mapa, impede colisão delas

--- playground/_mendel/public/game.js
+++ playground/_mendel/public/game.js
@@ -11,11 +11,48 @@ export default function createGame(subject) {
     const state = {
         players: {},
         fruits: {},
+        fruit_limit: 150,
         screen: {
-            width: 15,
-            height: 15
+            width: 2,
+            height: 2
         }
     }
+
+    // Carrega o ID do timer que gera as frutas do mapa
+    let spawnId = null
+
+    // Casa o nome da teclas que devem ter funcionalidade com o nome de suas respectivas funções. Aí é só chamar as funções com o próprio nome da tecla!
+    const acceptedMoves = {
+        // Utilizando as funções max e min, podemos nos livrar completamente dos ifs. Maravilhoso!
+        ArrowUp(player) {
+            player.y = Math.max(player.y - 1, 0)
+        },
+        ArrowDown(player) {
+            player.y = Math.min(player.y + 1, state.screen.height - 1)
+        },
+        ArrowLeft(player) {
+            player.x = Math.max(player.x - 1, 0)
+        },
+        ArrowRight(player) {
+            player.x = Math.min(player.x + 1, state.screen.width - 1)
+        }
+    }
+    // Define os "aliases"
+    Object.assign(acceptedMoves, {
+        w (player) {
+            acceptedMoves.ArrowUp(player)
+        },
+        a (player) {
+            acceptedMoves.ArrowLeft(player)
+        },
+        s (player) {
+            acceptedMoves.ArrowDown(player)
+        },
+        d (player) {
+            acceptedMoves.ArrowRight(player)
+        }
+    })
+
 
     // Implementação do design pattern: Observer
     // Funções como observer
@@ -80,11 +117,37 @@ export default function createGame(subject) {
     }
     
     function addFruit(command) {
+        // Verifica se já não estourou o limite de frutas
+        if (Object.keys(state.fruits).length >= state.fruit_limit) {
+            return
+        }
+        
         const fruitId = command ? command.fruitId : Math.floor(Math.random() * 10000000)
-        const fruitX = command ? command.fruitX : Math.floor(Math.random() * state.screen.width)
-        const fruitY = command ? command.fruitY : Math.floor(Math.random() * state.screen.height)
+        let fruitX = command ? command.fruitX : Math.floor(Math.random() * state.screen.width)
+        let fruitY = command ? command.fruitY : Math.floor(Math.random() * state.screen.height)
 
-        // console.log(`Adding ${fruitId} at x:${fruitX} y:${fruitY}`)
+        // Verifica se já existe uma fruta nesse local
+        let position_conflict = true
+        let tries = 0
+        while (position_conflict && tries < 5) {
+            position_conflict = false
+            for (const id in {...state.fruits, ...state.players}) {
+                const entity = state.fruits[id] ?? state.players[id]
+                if (fruitX == entity.x && fruitY == entity.y) {
+                    // console.log('[game] Tried spawning fruit in an occupied spot')
+                    tries++
+                    position_conflict = true
+                    fruitX = Math.floor(Math.random() * state.screen.width)
+                    fruitY = Math.floor(Math.random() * state.screen.height)
+                    break
+                }
+            }
+        }
+        if (tries == 5) {
+            return
+        }
+
+        console.log(`Adding ${fruitId} at x:${fruitX} y:${fruitY}`)
 
         state.fruits[fruitId] = {
             fruitId,
@@ -110,30 +173,14 @@ export default function createGame(subject) {
         })
     }
 
-    function start(frequency = 2000) {
-        setInterval(addFruit, frequency)
+    function spawnFruits(frequency = 2000) {
+        spawnId = setInterval(addFruit, frequency)
     }
 
     function movePlayer(command) {
         console.log(`> Moving ${command.playerId} with ${command.keyPressed}`)
         notifyAll(command)
         // Implementação do design pattern: Command
-        // Casa o nome da teclas que devem ter funcionalidade com o nome de suas respectivas funções. Aí é só chamar as funções com o próprio nome da tecla!
-        const acceptedMoves = {
-            // Utilizando as funções max e min, podemos nos livrar completamente dos ifs. Maravilhoso!
-            ArrowUp(player) {
-                player.y = Math.max(player.y - 1, 0)
-            },
-            ArrowDown(player) {
-                player.y = Math.min(player.y + 1, state.screen.height - 1)
-            },
-            ArrowLeft(player) {
-                player.x = Math.max(player.x - 1, 0)
-            },
-            ArrowRight(player) {
-                player.x = Math.min(player.x + 1, state.screen.width - 1)
-            },
-        }
 
         const moveFunction = acceptedMoves[command.keyPressed]
         const player = state.players[command.playerId]
@@ -158,12 +205,13 @@ export default function createGame(subject) {
     }
 
     return {
+        acceptedMoves,
         addPlayer,
         subscribe,
         removePlayer,
         addFruit,
         removeFruit,
-        start,
+        start: spawnFruits,
         movePlayer,
         state
     }
