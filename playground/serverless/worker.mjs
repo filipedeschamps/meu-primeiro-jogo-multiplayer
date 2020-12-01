c6d8034dada6203b7c764fb9d1db74cee7ac241d
Lucas Boemeke <lucas@blueshift.cc>
2020-11-30 23:55:13 -0300

    .

--- playground/serverless/worker.mjs
+++ playground/serverless/worker.mjs
@@ -235,34 +235,30 @@ export class ChatRoom {
   async fetch(request) {
     return await handleErrors(request, async () => {
       let url = new URL(request.url);
-
-      switch (url.pathname) {
-        case "/websocket": {
-          // The request is to `/api/room/<name>/websocket`. A client is trying to establish a new
-          // WebSocket session.
-          if (request.headers.get("Upgrade") != "websocket") {
-            return new Response("expected websocket", {status: 400});
-          }
-
-          // Get the client's IP address for use with the rate limiter.
-          let ip = request.headers.get("CF-Connecting-IP");
-
-          // To accept the WebSocket request, we create a WebSocketPair (which is like a socketpair,
-          // i.e. two WebSockets that talk to each other), we return one end of the pair in the
-          // response, and we operate on the other end. Note that this API is not part of the
-          // Fetch API standard; unfortunately, the Fetch API / Service Workers specs do not define
-          // any way to act as a WebSocket server today.
-          let pair = new WebSocketPair();
-
-          // We're going to take pair[1] as our end, and return pair[0] to the client.
-          await this.handleSession(pair[1], ip);
-
-          // Now we return the other end of the pair to the client.
-          return new Response(null, { status: 101, webSocket: pair[0] });
+      if (url.pathname.indexOf('/websocket') != -1) {
+        // The request is to `/api/room/<name>/websocket`. A client is trying to establish a new
+        // WebSocket session.
+        if (request.headers.get("Upgrade") != "websocket") {
+          return new Response("expected websocket", {status: 400});
         }
 
-        default:
-          return new Response("Not found", {status: 404});
+        // Get the client's IP address for use with the rate limiter.
+        let ip = request.headers.get("CF-Connecting-IP");
+
+        // To accept the WebSocket request, we create a WebSocketPair (which is like a socketpair,
+        // i.e. two WebSockets that talk to each other), we return one end of the pair in the
+        // response, and we operate on the other end. Note that this API is not part of the
+        // Fetch API standard; unfortunately, the Fetch API / Service Workers specs do not define
+        // any way to act as a WebSocket server today.
+        let pair = new WebSocketPair();
+
+        // We're going to take pair[1] as our end, and return pair[0] to the client.
+        await this.handleSession(pair[1], ip);
+
+        // Now we return the other end of the pair to the client.
+        return new Response(null, { status: 101, webSocket: pair[0] });
+      } else {
+        return new Response("Not found", {status: 404});
       }
     });
   }
